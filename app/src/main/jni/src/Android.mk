e85412fcd58e0bc0fa6adbf5205d2ccc4c495fb5
Anton Andell <anton@andell.eu>
2018-02-13 10:19:04 +0100

    some touch event test, working. library SDL2_image installation needed

--- app/src/main/jni/src/Android.mk
+++ app/src/main/jni/src/Android.mk
@@ -6,12 +6,12 @@ LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/../SDL2_image/
 
 # Add your application source files here...
 LOCAL_SRC_FILES := 52_hello_mobile.cpp
 
-LOCAL_SHARED_LIBRARIES := SDL2
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
